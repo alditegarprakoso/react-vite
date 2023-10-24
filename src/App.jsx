7f47fec7a0dc109f96c1e0d7476f547fd35b4105
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2023-10-24 13:33:56 +0700

    learn component and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 const Button = (props) => {
+  const { children = 'Insert title', color = 'bg-black' } = props;
+
   return (
     <button
-      className={`h-10 px-6 font-semibold rounded-md ${ props.color } text-white`}
+      className={`h-10 px-6 font-semibold rounded-md ${ color } text-white`}
       type="submit"
     >
-      { props.title }
+      { children }
     </button>
   );
 };
-
-function ButtonNew(props) {
-  return (
-    <button
-      className={`h-10 px-6 font-semibold rounded-md ${ props.color } text-white`}
-      type="submit"
-    >
-      { props.title }
-    </button>
-  )
-}
 
 function App() {
   return (
     <>
       <div className="flex justify-center bg-blue-400 min-h-screen items-center text-white">
         <div className="flex gap-x-2">
-          <Button title="Order Now" color="bg-black"></Button>
-          <Button title="Buy Now" color="bg-slate-600"></Button>
-          <Button title="Buy Now" color="bg-red-600"></Button>
-          <ButtonNew title="Button New" color="bg-red-400"></ButtonNew>
+          <Button color="bg-black">
+            Login
+          </Button>
+          <Button color="bg-slate-600">
+            Logout
+          </Button>
+          <Button color="bg-red-600">
+            Register
+          </Button>
+          <Button></Button>
         </div>
       </div>
     </>
