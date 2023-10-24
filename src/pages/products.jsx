1497d94dbc260091eac08f6825c06d974f6be252
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2023-10-24 17:51:57 +0700

    rendering list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,23 +1,52 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const dataProducts = [
+  {
+    id: 1,
+    image: "/images/shoes-1.webp",
+    title: "Shoes Chocolate",
+    price: "100.000",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
+  },
+  {
+    id: 2,
+    image: "/images/adidas.jpeg",
+    title: "Adidas",
+    price: "150.000",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
+  },
+  {
+    id: 3,
+    image: "/images/vans.jpeg",
+    title: "Vans",
+    price: "200.000",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
+  },
+  {
+    id: 4,
+    image: "/images/new-balance.jpg",
+    title: "New Balance",
+    price: "200.000",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
+  },
+];
+
 export default function ProductsPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.webp" />
-        <CardProduct.Body title="New Balance" price="100.000">
-          New Balance is the best brand
-        </CardProduct.Body>
-        <CardProduct.Footer />
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.webp" />
-        <CardProduct.Body title="Adidas" price="150.000">
-          Adidas is the best brand
-        </CardProduct.Body>
-        <CardProduct.Footer />
-      </CardProduct>
+    <div className="flex justify-center items-center min-h-screen mx-5">
+      {dataProducts.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.title} price={product.price}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer />
+        </CardProduct>
+      ))}
     </div>
   );
 }
