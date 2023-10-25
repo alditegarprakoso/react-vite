0a4009ab828949d5017df85af98020c06e43b7ba
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2023-10-25 14:23:28 +0700

    learn state and useState

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -21,7 +21,7 @@ function Body(props) {
     <div className="text-white font-semibold p-5">
       <a href="#">
         <h5 className="text-xl tracking-tight">{title}</h5>
-        <p className="text-sm mt-1 mb-4">Rp. {price}</p>
+        <p className="text-sm mt-1 mb-4">{price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0})}</p>
         <p className="font-normal text-justify text-slate-200">
           {children}
         </p>
@@ -30,12 +30,14 @@ function Body(props) {
   );
 }
 
-function Footer() {
+function Footer(props) {
+  const { isClick, product } = props;
   return (
     <div className="flex items-center justify-center w-full rounded-b-2xl">
       <Button
-        color="bg-blue-600"
+        color="bg-blue-400"
         customClass="w-full rounded-t-none rounded-b-2xl"
+        onClick={() => isClick (product)}
       >
         Add to cart
       </Button>
@@ -50,7 +52,7 @@ CardProduct.Footer = Footer;
 export default function CardProduct(props) {
   const { children } = props;
   return (
-    <div className="bg-gray-700 w-full max-w-sm shadow-md h-auto rounded-2xl mx-4">
+    <div className="bg-gray-700 w-[30%] max-w-sm shadow-md h-auto rounded-2xl mx-4 mb-10">
       {children}
     </div>
   );
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
@@ -6,7 +7,7 @@ const dataProducts = [
     id: 1,
     image: "/images/shoes-1.webp",
     title: "Shoes Chocolate",
-    price: "100.000",
+    price: 100000,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
   },
@@ -14,7 +15,7 @@ const dataProducts = [
     id: 2,
     image: "/images/adidas.jpeg",
     title: "Adidas",
-    price: "150.000",
+    price: 150000,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
   },
@@ -22,7 +23,7 @@ const dataProducts = [
     id: 3,
     image: "/images/vans.jpeg",
     title: "Vans",
-    price: "200.000",
+    price: 200000,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
   },
@@ -30,7 +31,23 @@ const dataProducts = [
     id: 4,
     image: "/images/new-balance.jpg",
     title: "New Balance",
-    price: "200.000",
+    price: 200000,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
+  },
+  {
+    id: 5,
+    image: "/images/vans.jpeg",
+    title: "Vanskuy",
+    price: 200000,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
+  },
+  {
+    id: 6,
+    image: "/images/new-balance.jpg",
+    title: "New Career",
+    price: 200000,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, reiciendis!",
   },
@@ -38,6 +55,7 @@ const dataProducts = [
 
 export default function ProductsPage() {
   const user = JSON.parse(localStorage.getItem("user"));
+  const [cart, setCart] = useState([]);
 
   if (!user) {
     window.location.href = "/login";
@@ -47,25 +65,111 @@ export default function ProductsPage() {
     localStorage.removeItem("user");
     window.location.href = "/login";
   }
-  
+
+  function handleAddToCart(product) {
+    if (cart.find((item) => item.id === product.id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, qty: 1 }]);
+    }
+  }
+
+  function handleDelete(index) {
+    const newCart = [...cart];
+    newCart.splice(index, 1);
+    setCart(newCart);
+    console.log(newCart);
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen mx-5">
+    <div className="flex flex-col justify-center items-center min-h-screen overflow-x-hidden">
       {user && (
-        <div className="flex justify-end items-center w-full h-16 px-5 mb-10 bg-blue-400 rounded-lg">
-          <p className="text-white font-bold -tracking-tighter mr-5">{user.email}</p>
-          <Button customClass="bg-slate-700" onClick={handleLogout}>Logout</Button>
+        <div className="flex justify-end items-center w-full h-16 px-5 mb-5 bg-blue-400 sticky top-0">
+          <p className="text-white font-bold -tracking-tighter mr-5">
+            {user.email}
+          </p>
+          <Button customClass="bg-slate-700" onClick={handleLogout}>
+            Logout
+          </Button>
         </div>
       )}
-      <div className="flex justify-center items-center">
-        {dataProducts.map((product) => (
-          <CardProduct key={product.id}>
-            <CardProduct.Header image={product.image} />
-            <CardProduct.Body title={product.title} price={product.price}>
-              {product.description}
-            </CardProduct.Body>
-            <CardProduct.Footer />
-          </CardProduct>
-        ))}
+      <div className="flex justify-center w-full px-10 py-8">
+        <div className="flex flex-wrap w-8/12">
+          {dataProducts.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.Header image={product.image} />
+              <CardProduct.Body title={product.title} price={product.price}>
+                {product.description}
+              </CardProduct.Body>
+              <CardProduct.Footer isClick={handleAddToCart} product={product} />
+            </CardProduct>
+          ))}
+        </div>
+        <div className="w-4/12 p-5">
+          {cart.length !== 0 ? (
+            <div className="h-[500px] overflow-y-auto">
+              <p className="text-blue-400 font-bold text-4xl mb-5">Cart</p>
+              <p className="mb-3">Your Products :</p>
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead>
+                  <tr>
+                    <th className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      No
+                    </th>
+                    <th className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Product
+                    </th>
+                    <th className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Price
+                    </th>
+                    <th className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      QTY
+                    </th>
+                    <th className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {cart.map((item, index) => (
+                    <tr key={index}>
+                      <td className="px-2 text-center py-4 whitespace-nowrap">
+                        {index + 1}
+                      </td>
+                      <td className="px-2 text-center py-4 whitespace-nowrap">
+                        {item.title}
+                      </td>
+                      <td className="px-2 text-center py-4 whitespace-nowrap">
+                        {(item.qty * item.price).toLocaleString("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                          minimumFractionDigits: 0,
+                        })}
+                      </td>
+                      <td className="px-2 text-center whitespace-nowrap">
+                        {item.qty}
+                      </td>
+                      <td className="px-2 text-center py-4 whitespace-nowrap">
+                        <Button
+                          color="bg-red-500"
+                          onClick={() => handleDelete(index)}
+                        >
+                          Delete
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <p className="text-blue-400 font-bold text-4xl mb-5">Cart Empty</p>
+          )}
+        </div>
       </div>
     </div>
   );
